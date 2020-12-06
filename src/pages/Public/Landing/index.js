188cf2258c4dfaf923d8f197c023c739e921c747
Hasibul Hasan <hasibul_uap@outlook.com>
2020-12-06 09:15:31 +0600

    Item types property add

--- src/pages/Public/Landing/index.js
+++ src/pages/Public/Landing/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 import TopSlider from "./TopSlider";
-import RandomProduct from "./RandomProduct"
+import RandomProduct from "./RandomProduct";
+import ItemType from "./ItemType";
 
 export default class Landing extends Component {
     render() {
         return (
             <div className="home-landing">
                 <TopSlider></TopSlider>
+                <ItemType></ItemType>
                 <RandomProduct></RandomProduct>
             </div>
         )
