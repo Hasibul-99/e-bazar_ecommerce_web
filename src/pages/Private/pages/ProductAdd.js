358227bb462d498c9aeca10793f83b4e5df4911b
Hasibul Hasan <hasibul_uap@outlook.com>
2021-01-18 08:59:08 +0600

    product add pge deign update

--- src/pages/Private/pages/ProductAdd.js
+++ src/pages/Private/pages/ProductAdd.js
@@ -37,7 +37,7 @@ export default class ProductAdd extends Component {
                                     </div>
                                     
                                     <div className="form-group col-6">
-                                        <label>SKU</label>
+                                        <label>SKU (Stock keeping unit)</label>
                                         <input type="text" className="form-control" />
                                     </div>
                                     <div className="form-group col-6">
@@ -49,15 +49,80 @@ export default class ProductAdd extends Component {
                                         <input type="text" className="form-control" />
                                     </div>
                                     
+                                    <div className="form-group col-6">
+                                        <label>Slider</label>
+                                        <select class="custom-select">
+                                            <option value="false">NO</option>
+                                            <option value="true">Yes</option>
+                                        </select>
+                                    </div>
+
+                                    <div className="form-group col-6">
+                                        <label>Flash Sall</label>
+                                        <select class="custom-select">
+                                            <option value="false">NO</option>
+                                            <option value="true">Yes</option>
+                                        </select>
+                                    </div>
+
+                                    <div className="form-group col-6">
+                                        <label>Bundle Offer</label>
+                                        <select class="custom-select">
+                                            <option value="false">NO</option>
+                                            <option value="true">Yes</option>
+                                        </select>
+                                    </div>
+
                                     <div className="form-group col-12 text-right">
                                         <button type="button" className="btn btn-square btn-outline-primary"  
                                             onClick={() => {this.setState({isShowFirstPart: false})}}>Save and Next</button>
                                     </div>
                                 </Fragment> : <Fragment>
-                                    <div className="form-group col-12">
+                                    {/* img 1 */}
+                                    <div className="form-group col-6">
                                         <label>Image</label>
-                                        <input type="file" className="form-control" />
+                                        <input type="file" id="js-product-img-1" className="form-control" />
                                     </div>
+                                    <div className="col-6">
+                                        <img className="img-thumbnail" id="js-product-img-view-1"  src="" alt="" />
+                                    </div>
+
+                                    {/* img 2 */}
+                                    <div className="form-group col-6">
+                                        <label>Image</label>
+                                        <input type="file" id="js-product-img-1" className="form-control" />
+                                    </div>
+                                    <div className="col-6">
+                                        <img className="img-thumbnail" id="js-product-img-view-1"  src="" alt="" />
+                                    </div>
+
+                                    {/* img 3 */}
+                                    <div className="form-group col-6">
+                                        <label>Image</label>
+                                        <input type="file" id="js-product-img-1" className="form-control" />
+                                    </div>
+                                    <div className="col-6">
+                                        <img className="img-thumbnail" id="js-product-img-view-1"  src="" alt="" />
+                                    </div>
+
+                                    {/* img 4 */}
+                                    <div className="form-group col-6">
+                                        <label>Image</label>
+                                        <input type="file" id="js-product-img-1" className="form-control" />
+                                    </div>
+                                    <div className="col-6">
+                                        <img className="img-thumbnail" id="js-product-img-view-1"  src="" alt="" />
+                                    </div>
+
+                                    {/* img 5  */}
+                                    <div className="form-group col-6">
+                                        <label>Image</label>
+                                        <input type="file" id="js-product-img-1" className="form-control" />
+                                    </div>
+                                    <div className="col-6">
+                                        <img className="img-thumbnail" id="js-product-img-view-1"  src="" alt="" />
+                                    </div>
+
                                     <div className="form-group col-12 text-right">
                                         <button type="button" className="btn btn-square btn-outline-success">Submit</button>
                                     </div>
