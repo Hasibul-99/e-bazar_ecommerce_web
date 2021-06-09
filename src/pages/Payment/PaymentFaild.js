a8428b53626bae0f075a66276ce76dbb30c3a16b
hasibul hasan <hasibul.hasan@sslwireless.com>
2021-06-09 15:52:51 +0600

    payments page design

--- src/pages/Payment/PaymentFaild.js
+++ src/pages/Payment/PaymentFaild.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function PaymentFaild() {
     return (
-        <div>
-            PaymentFaild
+        <div className="payment-failed">
+            <div className="card-failed">
+                <div style={{borderRadius: '200px', height: '200px', width: '200px', background: 'rgb(236 57 89 / 32%)', margin: '0 auto'}}>
+                    <i className="checkmark">!</i>
+                </div>
+                    <h1>Failed</h1> 
+                    <p>We received your purchase request;<br/> we'll be in touch shortly!</p>
+            </div>
         </div>
     )
 }
--- src/pages/Payment/PaymentSuccess.js
+++ src/pages/Payment/PaymentSuccess.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function PaymentSuccess() {
     return (
-        <div>
-            PaymentSuccess
+        <div className="payment-success">
+            <div className="card-success">
+                <div style={{borderRadius: '200px', height: '200px', width: '200px', background: '#F8FAF5', margin: '0 auto'}}>
+                    <i className="checkmark">✓</i>
+                </div>
+                    <h1>Success</h1> 
+                    <p>We received your purchase request;<br/> we'll be in touch shortly!</p>
+            </div>
         </div>
     )
 }
